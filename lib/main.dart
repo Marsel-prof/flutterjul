0b5747302edc1aa5f38a7da20d346d42756fccb1
Marsel Hanani <145753906+Marsel-prof@users.noreply.github.com>
2024-08-24 07:04:11 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:
+          false, // to hidden the debug banner let it false
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+// this is the build widget
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(
+          "Title" // i will write title here
+          ,
+          style: TextStyle(
+            backgroundColor: Color(0xFF00FF00), // to write hexa code color
+            // you should start with 0xFF
+          ), // detect style of text
+        ),
+        actions: [
+          // to make ayn widget clickable i have function named InkWell , GestureDetector
+          InkWell(
+            onTap: () => print("click me"),
+            child: const Text("clickable "),
+          ),
+          const Text("settings"), // to add text in appbar
+          const Icon(Icons.settings), // to add icon near to text
+          IconButton(
+            onPressed: () {
+              // write code dart
+            },
+            icon: const Icon(Icons.wifi),
+          )
+        ], // take list of widget
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
